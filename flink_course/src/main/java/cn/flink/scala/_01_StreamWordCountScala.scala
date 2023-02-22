2b4e40b1a855df9ff4202573fa900c3518ff47f5
taikun928 <1663177102@qq.com>
2023-02-23 01:35:22 +0800

    add java scala codes

--- flink_course/src/main/java/cn/flink/scala/_01_StreamWordCountScala.scala
+++ flink_course/src/main/java/cn/flink/scala/_01_StreamWordCountScala.scala
@@ -6,7 +6,7 @@ import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, createT
  * 通过socket数据源，去请求一个socket服务（doit01:9999）得到数据流
  * 然后统计数据流中出现的单词及其个数 scala 语言实现
  */
-object _01_入门程序WordCount {
+object _01_StreamWordCountScala {
   def main(args: Array[String]): Unit = {
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment
