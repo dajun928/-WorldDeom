f22e9f5eb12bbac3117d377bc078dcaf313376a4
taikun928 <1663177102@qq.com>
2023-03-03 00:02:27 +0800

    flink add code

---
+++ flink_course/src/main/java/cn/flinksql/UserAction.java
@@ -0,0 +1,20 @@
+package cn.flinksql;
+
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class UserAction implements Serializable {
+    // 2,1238598834956,pageview,20
+    private long guid;
+    private long actionTime;
+    private String eventId;
+    private long actionStaylong;
+    private String channel;
+}
